4a0d430164ec0a1b4cf18d7622110d006e3db8bc
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-01 01:07:36 +0300

    Start home page hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,17 +2,20 @@ import "./home.scss";
 import { Button } from "@mui/material";
 const Home = () => {
   return (
-    <div className=" home-wrapper relative bg-no-repeat bg-cover bg-center flex items-center justify-center ">
+    <div className="home-wrapper relative bg-no-repeat bg-cover bg-center flex items-center justify-center h-screen -mt-[5em] md:-mt-[4em]">
       <div className="absolute bg-black opacity-50 inset-0"></div>
       <div className="relative flex flex-col gap-4 text-white p-2 my-7 text-center items-center">
-        <h1 className="text-2xl font-semibold">
+        <h1 className="text leading-10 text-2xl font-semibold">
           Unlock Infinite Digital Potential.
         </h1>
-        <p>
+        <p className="max-w-[30em]">
           Elevate your digital journey with us. Seamless solutions, limitless
           possibilities. Experience innovation, tailored for you
         </p>
-        <Button variant="contained" className="w-[200px]">
+        <Button
+          variant="contained"
+          className="heroButton w-[120px] rounded-none"
+        >
           CTA
         </Button>
       </div>

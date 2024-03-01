d56417408256a931797b1bacfe94ae28f706894e
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-01 14:39:11 +0300

    finish responsiveness

--- src/shared/data/data.js
+++ src/shared/data/data.js
@@ -11,7 +11,7 @@ export const services = [
   {
     id: 2,
     image:
-      "https://img.freepik.com/free-vector/cloud-connection-abstract-concept-illustration_335657-3873.jpg?t=st=1709288253~exp=1709291853~hmac=ab41542d2428067bbc018ac3e75e724f7ddaf324ca5d52be87a31f4984f449ca&w=826",
+      "https://img.freepik.com/free-vector/gradient-website-hosting-illustration_23-2149247164.jpg?t=st=1709291585~exp=1709295185~hmac=4e4a552f4f45d807f31358c91c838e95f431245fff7ccd3700aedc438d6071d7&w=826",
     serviceName: "Object storage",
     description:
       "Store, access, and manage your data effortlessly with our reliable object storage solution. Enjoy high durability, low latency, and unlimited scalability, ensuring your critical information is always available when you need it most.",
@@ -23,7 +23,7 @@ export const services = [
       "https://img.freepik.com/free-vector/cloud-connection-abstract-concept-illustration_335657-3873.jpg?t=st=1709288253~exp=1709291853~hmac=ab41542d2428067bbc018ac3e75e724f7ddaf324ca5d52be87a31f4984f449ca&w=826",
     serviceName: "Array storage",
     description:
-      "Maximize efficiency and performance with our array storage service, designed to handle vast amounts of data with ease. Whether you're storing multimedia content or mission-critical databases, our array storage solution provides unmatched reliability and speed for your storage needs.",
+      "Maximize efficiency and performance with our array storage service, designed to handle vast amounts of data with ease. Our array storage solution provides unmatched reliability and speed for your storage needs.",
     path: "/array-storage",
   },
 ];

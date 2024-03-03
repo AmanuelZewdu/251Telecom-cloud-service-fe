98a2bd7635d3e218fb8d43ac14c310cb98c1a79b
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-03 21:57:27 +0300

    Remove commented lines and fix nav bar opening and closing issues

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,119 +1,3 @@
-// import "./navbar.scss";
-// import { useEffect, useState } from "react";
-// import MenuIcon from "@mui/icons-material/Menu";
-
-// const Navbar = () => {
-//   const [navIsOpen, setNavIsOpen] = useState(false);
-//   const [scrolled, setScrolled] = useState(false);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       setScrolled(window.scrollY > 0);
-//     };
-
-//     window.addEventListener("scroll", handleScroll);
-
-//     return () => {
-//       window.removeEventListener("scroll", handleScroll);
-//     };
-//   }, []);
-
-//   const handleNav = () => {
-//     setNavIsOpen(!navIsOpen);
-//   };
-//   // const navigate = useNavigate();
-//   const navigationLinks = [
-//     { path: "/", label: "Home" },
-//     { path: "/console", label: "Console" },
-//     { path: "/products", label: "Products" },
-//   ];
-//   const Link = ({ path, label, isActive }) => {
-//     return (
-//       <li key={path}>
-//         <a className="text-white" href={path}>
-//           {label}
-//         </a>
-//       </li>
-//     );
-//   };
-//   return (
-//     <div className="">
-//       <div
-//         className={`bg-black/20 ${navIsOpen ? "relative top-0 h-screen" : ""}`}
-//       ></div>
-//       <nav
-//         className={`fixed w-full flex justify-between items-center z-20 p-3 xl:px-[10rem] mx-auto shadow-xl bg-amber-500 ${
-//           scrolled ? "bg-amber-500/80 backdrop-blur-sm" : ""
-//         }`}
-//       >
-//         {" "}
-//         {/* Container for the logo */}
-//         <div className="w-[250px]">
-//           <img
-//             className="max-w-full"
-//             src={require("../../shared/images/251Logo.png")}
-//             alt="Your Company Logo"
-//           />
-//         </div>
-//         <div className="hidden md:block w-full">
-//           <ul
-//             className="justify-end"
-//             style={{ listStyle: "none", display: "flex" }}
-//           >
-//             {navigationLinks.map((link) => (
-//               <li key={link.path} style={{ marginRight: "10px" }}>
-//                 {" "}
-//                 {/* <Link to={link.path} className="nav-link">
-//                   {link.label}
-//                 </Link> */}
-//                 <Link
-//                   key={link.path}
-//                   path={link.path}
-//                   label={link.label}
-//                   isActive={link.active}
-//                 />
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//         <div className="">
-//           <div className="md:hidden text-white">
-//             <MenuIcon sx={{ fontSize: 32 }} onClick={handleNav} />
-//           </div>
-//           <div
-//             className={`slider fixed gap-4 bg-amber-500 top-0
-//            right-0 cursor-pointer transition-transform w-full duration-500 ease-in-out text-white
-//         ${navIsOpen ? "translate-y-20" : "-translate-y-full"}
-//         flex flex-col p-4 items-center`}
-//           >
-//             {/* <div className=" w-full flex justify-end">
-//               <CloseIcon onClick={() => setNavIsOpen(false)} />
-//             </div> */}
-//             <ul className="flex flex-col gap-4 text-center">
-//               {navigationLinks.map((link) => (
-//                 <li key={link.path} style={{ marginRight: "10px" }}>
-//                   {" "}
-//                   {/* <Link to={link.path} className="nav-link">
-//                   {link.label}
-//                 </Link> */}
-//                   <Link
-//                     key={link.path}
-//                     path={link.path}
-//                     label={link.label}
-//                     isActive={link.active}
-//                     onClick={handleNav}
-//                   />
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// };
-// export default Navbar;
-
 import "./navbar.scss";
 import { useEffect, useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -155,9 +39,12 @@ const Navbar = () => {
   };
 
   return (
-    <div onClick={() => setNavIsOpen(!navIsOpen)}>
+    <div>
       {navIsOpen && (
-        <div className="fixed top-0 left-0 w-full h-screen bg-black/50 z-10"></div>
+        <div
+          onClick={() => setNavIsOpen(!navIsOpen)}
+          className="fixed top-0 left-0 w-full h-screen bg-black/50 z-10"
+        ></div>
       )}
       <nav
         className={`fixed w-full flex justify-between items-center z-20 p-3 xl:px-[10rem] mx-auto shadow-xl bg-amber-500 ${
@@ -172,7 +59,7 @@ const Navbar = () => {
           />
         </div>
         <div className="hidden md:block w-full">
-          <ul className="flex justify-end" style={{ listStyle: "none" }}>
+          <ul className="flex justify-end gap-4" style={{ listStyle: "none" }}>
             {navigationLinks.map((link) => (
               <Link key={link.path} path={link.path} label={link.label} />
             ))}

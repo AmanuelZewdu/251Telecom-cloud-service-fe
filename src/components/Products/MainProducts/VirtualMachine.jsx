b188501cc2e6e33bf468f34a55ef81bb374868db
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-04 22:27:42 +0300

    Improve VM page styling

--- src/components/Products/MainProducts/VirtualMachine.jsx
+++ src/components/Products/MainProducts/VirtualMachine.jsx
@@ -1,13 +1,15 @@
 const VirtualMachine = () => {
   return (
-    <div className="h-screen w-full pt-6 text-center flex flex-col gap-12 p-2">
-      <h1>Virtual Machine</h1>
+    <div className="h-screen w-full pt-6 text-center flex flex-col gap-8 p-2 md:text-left">
+      <h1 className="text-2xl">Virtual Machine</h1>
       <div>
-        <div className="border shadow-md flex flex-col p-2">
-          <h2>Product details</h2>
+        <div className="border shadow-md flex text-left flex-col p-2 gap-2">
+          <h2 className="text-lg font-semibold font-montserrat">
+            Product details
+          </h2>
           <div className="flex items-start">
             <h4>
-              Product name: <span className="font-medium">Virtual Machine</span>
+              Product type: <span className="font-medium">Virtual Machine</span>
             </h4>
           </div>
         </div>

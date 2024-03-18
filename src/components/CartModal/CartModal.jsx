d4b84c97ba42bd649c56a1374ab6a1b1f9512df6
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-18 15:08:00 +0300

    Add checkout button

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -1,6 +1,8 @@
+import "./cartModal.scss";
 import { useState, useEffect } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Button } from "@mui/material";
 
 const CartModal = ({ open, onClose }) => {
   const [cartItems, setCartItems] = useState([]);
@@ -30,25 +32,13 @@ const CartModal = ({ open, onClose }) => {
     window.dispatchEvent(new Event("storage"));
   };
 
-  useEffect(() => {
-    if (open) {
-      document.body.classList.add("overflow-hidden");
-    } else {
-      document.body.classList.remove("overflow-hidden");
-    }
-
-    return () => {
-      document.body.classList.remove("overflow-hidden");
-    };
-  });
-
   return (
     <div
-      className={`fixed right-0 z-50 overflow-hidden ${
+      className={`fixed right-0 z-50  ${
         open ? "translate-x-0" : "translate-x-full"
       } transition-transform duration-500 shadow-[0px_0px_18px_2px_#c9c9c9] h-screen`}
     >
-      <div className="flex flex-col w-[20em] md:min-w-[26em] bg-white h-full p-4">
+      <div className="cartModal flex flex-col w-[20em] md:min-w-[26em] bg-white h-full p-4 gap-4 overflow-y-auto">
         <div className="flex mb-4 items-center justify-between">
           <h2 className="text-lg text-[#111111] font-poppins">Your Cart</h2>
           <button className="text-[#111111]" onClick={onClose}>
@@ -56,7 +46,7 @@ const CartModal = ({ open, onClose }) => {
           </button>
         </div>
         <hr />
-        <ul className="flex mt-4 flex-col gap-2">
+        <ul className="flex mt-4 flex-col gap-2 ">
           {cartItems.map((item, index) => (
             <li
               className="flex bg-gray-50 items-center justify-between p-2 rounded-md shadow-md"
@@ -89,7 +79,7 @@ const CartModal = ({ open, onClose }) => {
           ))}
         </ul>
         {cartItems.length > 0 && (
-          <div className="flex flex-col gap-2 mt-4">
+          <div className="flex flex-col gap-2">
             <h2 className="font-poppins">
               Items: <span className="font-medium">{cartItems.length}</span>
             </h2>
@@ -104,6 +94,16 @@ const CartModal = ({ open, onClose }) => {
             </h1>
           </div>
         )}
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: "#f59e0b",
+            padding: "0.5em",
+            color: "#fff",
+          }}
+        >
+          <a href="/checkout">Proceed to checkout</a>
+        </Button>
       </div>
     </div>
   );

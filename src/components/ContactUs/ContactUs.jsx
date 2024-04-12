e3697333b2db3b612f556fa5122f7ddc87c13190
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-04-12 16:01:59 +0300

    Finish contact us page for now

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,41 +1,37 @@
 import "./contactUs.scss";
 import { useState } from "react";
 import { useFormik } from "formik";
-import CloseIcon from "@mui/icons-material/Close";
 import * as Yup from "yup";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import VisibilityIcon from "@mui/icons-material/Visibility";
+import logo from "../../shared/images/cloud251Logo.png";
+
 const ContactUs = () => {
-  const [visible, setVisibile] = useState(false);
-  const [logInError, setLogInError] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleVisibility = () => {
-    setVisibile(!visible);
-  };
   const formik = useFormik({
     initialValues: {
       email: "",
-      password: "",
+      fullName: "",
       message: "",
     },
     validationSchema: Yup.object({
-      message: Yup.string().required("This field is required"),
+      fullName: Yup.string()
+        .min(8, "fullName must be 8 characters or more")
+        .required("Full name is required*"),
+      message: Yup.string()
+        .min(12, "Message must contain at least 12 characters.")
+        .required("This field is required"),
       email: Yup.string()
         .matches(
           /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
           "Invalid email address"
         )
         .required("Email address is required*"),
-      password: Yup.string()
-        .min(8, "Password must be 8 characters or more")
-        .required("Password is required*"),
     }),
     onSubmit: async (values) => {},
   });
 
   return (
-    <div className="flex flex-col w-full place-content-center font-montserrat">
+    <div className="flex flex-col gap-4 w-full place-content-center font-montserrat mb-8">
       <div className="contactUs w-full flex flex-col place-items-center place-content-end min-h-[19em] p-8 text-center gap-4">
         <h1 className="text-4xl text-primary-blue font-semibold">
           Contact <span className="relative text-button-color line">us</span>
@@ -45,105 +41,111 @@ const ContactUs = () => {
           hesitate to reach out with any questions, comments, or concerns..
         </span>
       </div>
-      <div>
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col gap-4 text-black"
-        >
-          <div className="w-full">
-            <label htmlFor="email" className="text-gray-900">
-              Email address:
-            </label>
-            <input
-              className={`w-full bg-transparent p-2 border rounded-sm outline-none ${
-                formik.touched.email && formik.errors.email
-                  ? "border-red-500"
-                  : formik.touched.email && !formik.errors.email
-                  ? "border-green-500"
-                  : "border-gray-400"
-              }`}
-              id="email"
-              placeholder="Email"
-              type="email"
-              {...formik.getFieldProps("email")}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <div className="text-red-600 text-xs mt-1">
-                {formik.errors.email}
-              </div>
-            ) : null}
+      <div className="mx-4 flex flex-col xl:flex-row gap-4 xl:gap-0 items-center justify-center">
+        <div className="relative flex flex-col  xl:self-start items-center w-full gap-4 p-4 justify-center text-center xl:ml-[10em]">
+          <div className="max-w-[8em]">
+            <img className="w-full" src={logo} alt="" />
           </div>
-          <div className="relative w-full">
-            <label htmlFor="password" className="text-gray-900">
-              Password:
-            </label>
-            <input
-              className={`w-full bg-transparent p-2 border rounded-sm outline-none ${
-                formik.touched.password && formik.errors.password
-                  ? "border-red-500"
-                  : formik.touched.password && !formik.errors.password
-                  ? "border-green-500"
-                  : "border-gray-400"
-              }`}
-              id="password"
-              placeholder="Password"
-              type={visible ? "text" : "password"}
-              {...formik.getFieldProps("password")}
-            />
-            {visible ? (
-              <VisibilityIcon
-                onClick={handleVisibility}
-                className="absolute top-11 right-3 transform -translate-y-1/2 text-gray-500"
-              />
-            ) : (
-              <VisibilityOffIcon
-                onClick={handleVisibility}
-                className="absolute top-11 right-3 transform -translate-y-1/2 text-gray-500"
-              />
-            )}
-
-            {formik.touched.password && formik.errors.password ? (
-              <div className="text-red-600 text-xs mt-1">
-                {formik.errors.password}
-              </div>
-            ) : null}
-          </div>
-          <div className="w-full">
-            <label htmlFor="email" className="text-gray-900">
-              What can we help you with?
-            </label>
-            <input
-              className={`w-full bg-transparent p-2 border rounded-sm outline-none ${
-                formik.touched.message && formik.errors.message
-                  ? "border-red-500"
-                  : formik.touched.message && !formik.errors.message
-                  ? "border-green-500"
-                  : "border-gray-400"
-              }`}
-              id="message"
-              placeholder="Message"
-              type="text"
-              {...formik.getFieldProps("message")}
-            />
-            {formik.touched.message && formik.errors.message ? (
-              <div className="text-red-600 text-xs mt-1">
-                {formik.errors.message}
-              </div>
-            ) : null}
-          </div>
-          <button
-            className={`flex justify-center items-center gap-2 w-full rounded-sm  p-2 text-white  ${
-              loading
-                ? "bg-button-color/50 cursor-wait"
-                : "bg-button-color hover:bg-button-color"
-            }`}
-            type="submit"
-            disabled={loading}
+          <p className="max-w-[30em] text-lg text-gray-800">
+            Welcome to{" "}
+            <span className="bg-gradient-to-r from-primary-blue  via-secondary-blue to-primary-blue text-transparent bg-clip-text font-semibold text-xl">
+              Cloud251!
+            </span>
+            , We are located at Meskel flower, Addis Ababa, Ethiopia. Feel free
+            to visit us during our business hours or contact us for any
+            inquiries. We look forward to seeing you!
+          </p>
+        </div>
+        <div className="w-full flex items-center justify-center xl:justify-start">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex w-full max-w-[30em] flex-col gap-4 text-white border p-6 bg-gradient-to-br from-primary-blue to-secondary-blue rounded-lg shadow-lg"
           >
-            {loading ? "Logging in" : "Log in"}
-            {loading && <div className="login-Loader"></div>}
-          </button>
-        </form>
+            <div>
+              <h1 className="text-2xl font-semibold">Get in touch</h1>
+            </div>
+            <hr />
+            <div className="relative w-full">
+              <label htmlFor="fullName" className="">
+                Full name:
+              </label>
+              <input
+                className={`w-full bg-transparent p-2 border rounded- outline-none placeholder:text-gray-300 ${
+                  formik.touched.fullName && formik.errors.fullName
+                    ? "border-red-500"
+                    : formik.touched.fullName && !formik.errors.fullName
+                    ? "border-green-500"
+                    : "border-gray-400"
+                }`}
+                id="fullName"
+                placeholder="Full name"
+                type="text"
+                {...formik.getFieldProps("fullName")}
+              />
+
+              {formik.touched.fullName && formik.errors.fullName ? (
+                <div className="text-red-500  mt-1">
+                  {formik.errors.fullName}
+                </div>
+              ) : null}
+            </div>
+            <div className="w-full">
+              <label htmlFor="email" className="">
+                Email address:
+              </label>
+              <input
+                className={`w-full bg-transparent p-2 border rounded- outline-none placeholder:text-gray-300  ${
+                  formik.touched.email && formik.errors.email
+                    ? "border-red-500"
+                    : formik.touched.email && !formik.errors.email
+                    ? "border-green-500"
+                    : "border-gray-400"
+                }`}
+                id="email"
+                placeholder="Email"
+                type="email"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <div className="text-red-500  mt-1">{formik.errors.email}</div>
+              ) : null}
+            </div>
+            <div className="w-full">
+              <label htmlFor="email" className="">
+                What can we help you with?
+              </label>
+              <textarea
+                className={`w-full bg-transparent p-2 border rounded- outline-none placeholder:text-gray-300  ${
+                  formik.touched.message && formik.errors.message
+                    ? "border-red-500"
+                    : formik.touched.message && !formik.errors.message
+                    ? "border-green-500"
+                    : "border-gray-400"
+                }`}
+                id="message"
+                placeholder="Message"
+                {...formik.getFieldProps("message")}
+              />
+              {formik.touched.message && formik.errors.message ? (
+                <div className="text-red-500  mt-1">
+                  {formik.errors.message}
+                </div>
+              ) : null}
+            </div>
+            <button
+              className={`flex justify-center items-center gap-2 w-full rounded-md p-2 text-white shadow-lg ${
+                loading
+                  ? "bg-button-color/50 cursor-wait"
+                  : "bg-button-color hover:bg-button-color"
+              }`}
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? "Submitting" : "Submut"}
+              {loading && <div className="login-Loader"></div>}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );

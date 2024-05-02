7a5af2dcce4cb58a035f9f02965e243b08c61b86
Abdulahi <mmmabdulahi954@yahoo.com>
2024-05-02 16:14:18 +0300

    Make array and obj storage page under dev

--- src/components/Products/MainProducts/ArrayStorage.jsx
+++ src/components/Products/MainProducts/ArrayStorage.jsx
@@ -1,4 +1,27 @@
+import Lottie from "react-lottie";
+import * as animationData from "../../../shared/webDev.json";
+
 const ArrayStorage = () => {
-  return <div className="h-screen w-full text-center p-6">ArrayStorage</div>;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="mt-8 flex flex-col items-center justify-center gap-2">
+        <h1 className="text-3xl font-Inter font-bold text-primary-blue">
+          Under Development
+        </h1>
+        <h3 className="text-xl font-bold text-gray-300">Coming Soon</h3>
+      </div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+  );
 };
+
 export default ArrayStorage;

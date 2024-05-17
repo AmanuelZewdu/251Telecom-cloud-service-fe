55021e553b749c33311c30c277592c867e8d3e46
Abdulahi <mmmabdulahi954@yahoo.com>
2024-05-17 12:25:12 +0300

    FIix obj storage page UI responsiveness

--- src/components/Products/MainProducts/ObjectStorage.jsx
+++ src/components/Products/MainProducts/ObjectStorage.jsx
@@ -311,13 +311,15 @@ const ObjectStorage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="mt-8 flex flex-col items-center justify-center gap-2">
+      <div className="mt-[4em] md:mt-[7em] flex flex-col items-center justify-center gap-2">
         <h1 className="text-3xl font-Inter font-bold text-primary-blue">
           Under Development
         </h1>
         <h3 className="text-xl font-bold text-gray-300">Coming Soon</h3>
       </div>
-      <Lottie options={defaultOptions} height={400} width={400} />
+      <div>
+        <Lottie options={defaultOptions} className="w-[10em]" />
+      </div>
     </div>
   );
 };
